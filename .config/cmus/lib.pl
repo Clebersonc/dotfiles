2e6e3becbc36988c938a677b05a00a873dc15988
Clebersonc <clebersongs@gmail.com>
2018-09-18 09:19:01 -0300

    Várias mudanças após nova instalação do arch

---
+++ .config/cmus/lib.pl
@@ -0,0 +1,176 @@
+/home/ctroy/Music/8 Bit Weapon/8 Bit Weapon - Bits With Byte.mp3
+/home/ctroy/Music/8 Bit Weapon/8 Bit Weapon - Galactic Invasion.mp3
+/home/ctroy/Music/8 Bit Weapon/8 Bit Weapon - The Art of Video Games Anthem.mp3
+/home/ctroy/Music/8 Bit Weapon/8 Bit Weapon - Miami Dub Bounce.mp3
+/home/ctroy/Music/8 Bit Weapon/8 Bit Weapon - We Fight for the Users.mp3
+/home/ctroy/Music/8 Bit Weapon/8 Bit Weapon - Drive Grinder.mp3
+/home/ctroy/Music/8 Bit Weapon/8 Bit Weapon - Escape From Xenon.mp3
+/home/ctroy/Music/8 Bit Weapon/8 Bit Weapon - Closer 20.mp3
+/home/ctroy/Music/8 Bit Weapon/8 Bit Weapon - Chip On Your Shoulder (Electric High Mix).mp3
+/home/ctroy/Music/8 Bit Weapon/8 Bit Weapon - Closer (8 Bit Bandit Remix).mp3
+/home/ctroy/Music/8 Bit Weapon/8 Bit Weapon - Chip On Your Shoulder (Sanxion7 Remix).mp3
+/home/ctroy/Music/8 Bit Weapon/8 Bit Weapon - Vic XX.mp3
+/home/ctroy/Music/Bignic/Bignic - romp.mp3
+/home/ctroy/Music/Bignic/Bignic - purpose.mp3
+/home/ctroy/Music/Bignic/Bignic - classic.mp3
+/home/ctroy/Music/Bignic/Bignic - boxes.mp3
+/home/ctroy/Music/Bignic/Bignic - bleed.mp3
+/home/ctroy/Music/Bignic/Bignic - moment.mp3
+/home/ctroy/Music/Bignic/Bignic - help.mp3
+/home/ctroy/Music/Bignic/Bignic - children of dune.mp3
+/home/ctroy/Music/Carpenter Brut/Carpenter Brut - Disco Zombi Italia.mp3
+/home/ctroy/Music/Carpenter Brut/Carpenter Brut - LA Venice Bitch 80s.mp3
+/home/ctroy/Music/Carpenter Brut/Carpenter Brut - Wake Up The President.mp3
+/home/ctroy/Music/Carpenter Brut/Carpenter Brut - 347 Midnight Demons.mp3
+/home/ctroy/Music/Carpenter Brut/Carpenter Brut - Le perv.mp3
+/home/ctroy/Music/Carpenter Brut/Carpenter Brut - Obituary.mp3
+/home/ctroy/Music/Carpenter Brut/Carpenter Brut - Looking For Tracy Tzu.mp3
+/home/ctroy/Music/Carpenter Brut/Carpenter Brut - Sexkiller On The Loose.mp3
+/home/ctroy/Music/Carpenter Brut/Carpenter Brut - Division Ruine.mp3
+/home/ctroy/Music/Carpenter Brut/Carpenter Brut - Paradise Warfare.mp3
+/home/ctroy/Music/Carpenter Brut/Carpenter Brut - Run Sally Run.mp3
+/home/ctroy/Music/Carpenter Brut/Carpenter Brut - Turbo Killer.mp3
+/home/ctroy/Music/Carpenter Brut/Carpenter Brut - Anarchy Road.mp3
+/home/ctroy/Music/Carpenter Brut/Carpenter Brut - Invasion AD.mp3
+/home/ctroy/Music/Chronotorious/CHRONOTORIOUS.mp3
+/home/ctroy/Music/Chronotorious/Castle Rock.mp3
+/home/ctroy/Music/Chronotorious/Rockin on Heavens Door.mp3
+/home/ctroy/Music/Chronotorious/Dethfrog.mp3
+/home/ctroy/Music/Chronotorious/Cave Girl.mp3
+/home/ctroy/Music/Chronotorious/BAMF.mp3
+/home/ctroy/Music/Chronotorious/Disodium guanylate.mp3
+/home/ctroy/Music/Chronotorious/Watertite.mp3
+/home/ctroy/Music/Chronotorious/Dream of Black.mp3
+/home/ctroy/Music/Chronotorious/Bottomed Out.mp3
+/home/ctroy/Music/Chronotorious/Longing of The Wind.mp3
+/home/ctroy/Music/Chronotorious/Forced Enlightenment.mp3
+/home/ctroy/Music/Chronotorious/Tyrannosaurus.mp3
+/home/ctroy/Music/Chronotorious/Forest Steppin.mp3
+/home/ctroy/Music/Chronotorious/The Bad Ending.mp3
+/home/ctroy/Music/Covox/Covox - Adult Checksum Corrupt.mp3
+/home/ctroy/Music/Covox/Covox - Attack Vector.mp3
+/home/ctroy/Music/Covox/Covox - A Billion Suns.mp3
+/home/ctroy/Music/Disasterpeace/Disasterpeace - Press Start - Prelude To An Unfolding Conflict.mp3
+/home/ctroy/Music/Disasterpeace/Disasterpeace - Kevlar Cove - Tales Of An Old Friend.mp3
+/home/ctroy/Music/Dubmood/Dubmood - NFL Crackintro Film.mp3
+/home/ctroy/Music/Dubmood/Dubmood - 17kr Sexpacket (Installer Edit).mp3
+/home/ctroy/Music/Dubmood/Dubmood - A Message.mp3
+/home/ctroy/Music/Dubmood/Dubmood - WoTW - The One and Only.mp3
+/home/ctroy/Music/Dubmood/Dubmood - with Zabutom - Supersquatting (Razor1911 Intro Edit).mp3
+/home/ctroy/Music/Dubmood/Dubmood - Flame of Finland (Warp Remix).mp3
+/home/ctroy/Music/Magic Sword/Magic Sword - The Beginning.mp3
+/home/ctroy/Music/Magic Sword/Magic Sword - Sword of Truth.mp3
+/home/ctroy/Music/Magic Sword/Magic Sword - Kill Them All.mp3
+/home/ctroy/Music/Magic Sword/Magic Sword - In The Face Of Evil.mp3
+/home/ctroy/Music/Magic Sword/Magic Sword - Reflection.mp3
+/home/ctroy/Music/Magic Sword/Magic Sword - Retrogram.mp3
+/home/ctroy/Music/Magic Sword/Magic Sword - Discover.mp3
+/home/ctroy/Music/Magic Sword/Magic Sword - Memories In Shadow.mp3
+/home/ctroy/Music/Magic Sword/Magic Sword - Battlefield.mp3
+/home/ctroy/Music/Magic Sword/Magic Sword - Journeys End.mp3
+/home/ctroy/Music/MisfitChris/MisfitChris - Speed Run.mp3
+/home/ctroy/Music/MisfitChris/MisfitChris - New York Weekends.mp3
+/home/ctroy/Music/MisfitChris/MisfitChris - SID Chip Birthday (Dubmood Remix).mp3
+/home/ctroy/Music/moon/MOON - Paris.mp3
+/home/ctroy/Music/moon/MOON - Crystals.mp3
+/home/ctroy/Music/moon/MOON - Hydrogen.mp3
+/home/ctroy/Music/moon/MOON - Medium Cool.mp3
+/home/ctroy/Music/moon/MOON - Finale.mp3
+/home/ctroy/Music/Patric Catani/Patric Catani - Blox in Space - Opener.mp3
+/home/ctroy/Music/Patric Catani/Patric Catani - Battleblock Theater - Wicked Mummy.mp3
+/home/ctroy/Music/Patric Catani/Patric Catani - Battleblock Theater - Panic Version.mp3
+/home/ctroy/Music/Patric Catani/Patric Catani - Blox in Space - Tetris Life.mp3
+/home/ctroy/Music/Patric Catani/Patric Catani - Battleblock Theater - Dream of Freedom.mp3
+/home/ctroy/Music/Rymdkraft/Rymdkraft - Kantarelle.mp3
+/home/ctroy/Music/Rymdkraft/Rymdkraft - Freon.mp3
+/home/ctroy/Music/Rymdkraft/Rymdkraft - Mustaschvapnet.mp3
+/home/ctroy/Music/Rymdkraft/Rymdkraft - Ultramumie.mp3
+/home/ctroy/Music/Rymdkraft/Rymdkraft - Smuggeldans 3000.mp3
+/home/ctroy/Music/Rymdkraft/Rymdkraft - Lukas och lianen.mp3
+/home/ctroy/Music/Rymdkraft/Rymdkraft - Fingersving.mp3
+/home/ctroy/Music/Rymdkraft/Rymdkraft - Sprinkler.mp3
+/home/ctroy/Music/Rymdkraft/Rymdkraft - Superduperkomputer.mp3
+/home/ctroy/Music/Rymdkraft/Rymdkraft - Diraffe.mp3
+/home/ctroy/Music/Rymdkraft/Rymdkraft - Alfa Beta Gunnar.mp3
+/home/ctroy/Music/Rymdkraft/Rymdkraft - Lets Golv.mp3
+/home/ctroy/Music/Rymdkraft/Rymdkraft - Koufukuron v3.mp3
+/home/ctroy/Music/Rymdreglage/Rymdreglage - Rated 75.mp3
+/home/ctroy/Music/Rymdreglage/Rymdreglage - Skaraslättens Transport.mp3
+/home/ctroy/Music/Rymdreglage/Rymdreglage - Alpaca Tango.mp3
+/home/ctroy/Music/Rymdreglage/Rymdreglage - Lead Man.mp3
+/home/ctroy/Music/Rymdreglage/Rymdreglage - Not Like Flowers.mp3
+/home/ctroy/Music/Rymdreglage/Rymdreglage - Brain Dead.mp3
+/home/ctroy/Music/Savant/Savant - Man of The Law - Original Mix.mp3
+/home/ctroy/Music/Savant/Savant - Prototype - Original Mix.mp3
+/home/ctroy/Music/Savant/Savant - Spaceship - Original Mix.mp3
+/home/ctroy/Music/Savant/Savant - Rider In Red - Original Mix.mp3
+/home/ctroy/Music/Savant/Savant - Fakers - Original Mix.mp3
+/home/ctroy/Music/Savant/Savant - Rise Up - Original Mix.mp3
+/home/ctroy/Music/Savant/Savant - Spaceheart - Original Mix.mp3
+/home/ctroy/Music/Savant/Savant - Laser Sharks - Original Mix.mp3
+/home/ctroy/Music/Savant/Savant - Cry For Love - Original Mix.mp3
+/home/ctroy/Music/Savant/Savant - Samurai - Original Mix.mp3
+/home/ctroy/Music/Savant/Savant - Quest - Original Mix.mp3
+/home/ctroy/Music/Savant/Savant - Venom - Original Mix.mp3
+/home/ctroy/Music/Savant/Savant - Super Sheriff - Original Mix.mp3
+/home/ctroy/Music/Savant/Savant - Sword of Destiny - Original Mix.mp3
+/home/ctroy/Music/Savant/Savant - Hit The Top - Original Mix.mp3
+/home/ctroy/Music/Savant/Savant - Aquarius - Original Mix.mp3
+/home/ctroy/Music/Savant/Savant - Beautiful World - Original Mix.mp3
+/home/ctroy/Music/Savoy/Savoy - Mind Over Matter.mp3
+/home/ctroy/Music/Savoy/Savoy - Bizzy.mp3
+/home/ctroy/Music/Savoy/Savoy - Cata (feat Lobounce).mp3
+/home/ctroy/Music/Savoy/Savoy - Perfect World.mp3
+/home/ctroy/Music/Savoy/Savoy - Queen.mp3
+/home/ctroy/Music/Savoy/Savoy - Ugh.mp3
+/home/ctroy/Music/Savoy/Savoy - R  R.mp3
+/home/ctroy/Music/Savoy/Savoy - So Bad (feat Bright Lights).mp3
+/home/ctroy/Music/Savoy/Savoy - Guesswork.mp3
+/home/ctroy/Music/Savoy/Savoy - Prey for New York.mp3
+/home/ctroy/Music/Savoy/Savoy - Nothing Stays the Same (feat Lobounce).mp3
+/home/ctroy/Music/The OneUps/The OneUps - Funky OHare [Bucky OHare].mp3
+/home/ctroy/Music/The OneUps/The OneUps - Saw VIII [Mega Man 2].mp3
+/home/ctroy/Music/The OneUps/The OneUps - Port Authority [F-Zero].mp3
+/home/ctroy/Music/The OneUps/The OneUps - Ice Ice Cavey [Donkey Kong Country].mp3
+/home/ctroy/Music/The OneUps/The OneUps - Silence is Golden Snitches Get Stitches [F-Zero].mp3
+/home/ctroy/Music/The OneUps/The OneUps - From Mt Itoi With Love [EarthBound].mp3
+/home/ctroy/Music/The OneUps/The OneUps - PC LOAD LETTER [Command  Conquer Red Alert].mp3
+/home/ctroy/Music/The OneUps/The OneUps - Chillin in the Underground [Super Mario Bros].mp3
+/home/ctroy/Music/The OneUps/The OneUps - POW Block [Super Mario Bros 2].mp3
+/home/ctroy/Music/Tool-Aenima/01 - stinkfist.mp3
+/home/ctroy/Music/Tool-Aenima/02 - eulogy.mp3
+/home/ctroy/Music/Tool-Aenima/03 - h..mp3
+/home/ctroy/Music/Tool-Aenima/04 - useful idiot.mp3
+/home/ctroy/Music/Tool-Aenima/05 - forty six & 2.mp3
+/home/ctroy/Music/Tool-Aenima/06 - message to harry manback.mp3
+/home/ctroy/Music/Tool-Aenima/07 - hooker with a penis.mp3
+/home/ctroy/Music/Tool-Aenima/08 - intermission.mp3
+/home/ctroy/Music/Tool-Aenima/09 - jimmy.mp3
+/home/ctroy/Music/Tool-Aenima/10 - die eier von satan.mp3
+/home/ctroy/Music/Tool-Aenima/11 - pushit.mp3
+/home/ctroy/Music/Tool-Aenima/12 - cesaro summability.mp3
+/home/ctroy/Music/Tool-Aenima/13 - ænema.mp3
+/home/ctroy/Music/Tool-Aenima/14 - (-) ions.mp3
+/home/ctroy/Music/Tool-Aenima/15 - third eye.mp3
+/home/ctroy/Music/Vomitron/Vomitron - Filtered Blow.mp3
+/home/ctroy/Music/Vomitron/Vomitron - Contra.mp3
+/home/ctroy/Music/Vomitron/Vomitron - Blaster Master.mp3
+/home/ctroy/Music/Vomitron/Vomitron - Kalinka.mp3
+/home/ctroy/Music/Vomitron/Vomitron - The Legend of Zelda.mp3
+/home/ctroy/Music/Vomitron/Vomitron - Ninja Gaiden Acts 1-3.mp3
+/home/ctroy/Music/Vomitron/Vomitron - Troika.mp3
+/home/ctroy/Music/Vomitron/Vomitron - Double Dragon.mp3
+/home/ctroy/Music/Vomitron/Vomitron - Bradinsky.mp3
+/home/ctroy/Music/Vomitron/Vomitron - Ninja Gaiden Acts 4-6.mp3
+/home/ctroy/Music/Vomitron/Vomitron - The Soviet Mind Game.mp3
+/home/ctroy/Music/Vomitron/Vomitron - Zelda II The Adventure of Link.mp3
+/home/ctroy/Music/Vomitron/Vomitron - Korobeyniki.mp3
+/home/ctroy/Music/Vomitron/Vomitron - Wizards  Warriors.mp3
+/home/ctroy/Music/Vomitron/Vomitron - Metroid (1 of 2).mp3
+/home/ctroy/Music/Vomitron/Vomitron - Teenage Mutant Ninja Turtles (1 of 2).mp3
+/home/ctroy/Music/Vomitron/Vomitron - Castlevania II Simons Quest.mp3
+/home/ctroy/Music/Vomitron/Vomitron - Bionic Commando (1 of 2).mp3
+/home/ctroy/Music/Vomitron/Vomitron - Metroid (2 of 2).mp3
+/home/ctroy/Music/Vomitron/Vomitron - Marble Madness.mp3
+/home/ctroy/Music/Vomitron/Vomitron - Teenage Mutant Ninja Turtles (2 of 2).mp3
+/home/ctroy/Music/Vomitron/Vomitron - Bionic Commando (2 of 2).mp3
--- .vim/plugin/packages/vimcompleteme.vim
+++
@@ -1,6 +0,0 @@
-autocmd FileType java,vim let b:vcm_tab_complete = 'eclim#java#complete#CodeComplete'
-" let b:vcm_tab_complete = 'omni'
-
-set completeopt-=preview
-set completeopt+=menu,menuone,noinsert,noselect
-set shortmess+=c
